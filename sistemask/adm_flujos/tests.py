51799f435ac7720da85f8069bd2f1d073cd454b4
rubenmed <rubencho.mecab@gmail.com>
2015-04-11 01:04:09 -0400

    documentacion de flujos.views, de flujos.tests y de flujos.models y tests

--- sistemask/adm_flujos/tests.py
+++ sistemask/adm_flujos/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from models import Flujo
+from adm_proyectos.models import Proyecto
 
 # Create your tests here.
+
+class FlujoTest(TestCase):
+
+    '''
+    Esta clase hace un prueba de creacion, modificacion y eliminacion de flujos.
+    Los campos del flujo son:
+        nombre: nombre del flujo;
+        descripcion: descripcion breve del flujo;
+        proyecto: proyecto al que pertenece el flujo;
+        activo: determina si el flujo esta activo o no en el proyecto.
+    '''
+
+    def crear(self):
+
+        flujo1 = Flujo.objects.create(nombre='Flujo Desarrollo 1', descripcion='Flujo Prueba 1', proyecto=Proyecto.objects.create(nombre='Proyecto 1'), activo=True)
+        flujo2 = Flujo.objects.create(nombre='Flujo Desarrollo 2', descripcion='Flujo Prueba 2', proyecto=Proyecto.objects.create(nombre='Proyecto 2'), activo=True)
+
+    def modificar(self):
+
+        flujo1 = Flujo.objects.update(nombre='Flujo Desarrollo 1.1')
+        flujo2 = Flujo.objects.update(nombre='Flujo Desarrollo 2.1')
+
+    def eliminar(self):
+
+        flujo1 = Flujo.objects.delete()
+        flujo2 = Flujo.objects.delete()
--- sistemask/adm_flujos/views.py
+++ sistemask/adm_flujos/views.py
@@ -8,8 +8,23 @@ from adm_usuarios.models import Usuario
 
 class FlujoView(TemplateView):
 
+    '''
+    Esta clase muestra los flujos en el proyecto.
+
+    Hereda de TemplateView.
+
+    template_name es el archivo html de esta clase.
+    '''
+
     template_name = 'Flujo.html'
     def post(self, request, *args, **kwargs):
+        '''
+        Esta funcion tiene los parametros:
+        :param request:
+        :param args:
+        :param kwargs:
+        :return: request, el arhivo html y el diccionario.
+        '''
         diccionario={}
         #usuario_logueado= Usuario.objects.get(id= request.POST['login'])
         #proyecto_actual= Proyecto.objects.get(id= request.POST['proyecto'])
@@ -23,10 +38,26 @@ class FlujoView(TemplateView):
 
 
 class CrearFlujo(FlujoView):
+    '''
+    Esta clase crea los flujos en el proyecto.
+
+    Hereda de FlujoView.
+
+    template_name es el archivo html de esta clase.
+    '''
 
     template_name = 'CrearFlujo.html'
 
     def post(self, request, *args, **kwargs):
+
+        '''
+        Esta funcion tiene los parametros:
+
+        :param request:
+        :param args:
+        :param kwargs:
+        :return: request, el archivo html y el diccionario
+        '''
         diccionario={}
         #usuario_logueado= Usuario.objects.get(id= request.POST['login'])
         #proyecto_actual= Proyecto.objects.get(id= request.POST['proyecto'])
@@ -38,8 +69,25 @@ class CrearFlujo(FlujoView):
 
 class CrearFlujoConfirm(CrearFlujo):
 
+    '''
+    Esta clase confirma la creacion de los flujos en el proyecto.
+
+    Hereda de CrearFlujo
+
+    template_name es el archivo html de esta clase.
+    '''
+
     template_name = 'CrearFlujoConfirmar.html'
     def post(self, request, *args, **kwargs):
+        '''
+        Esta funcion tiene los parametros:
+
+        :param request:
+        :param args:
+        :param kwargs:
+        :return: request y el archivo html
+        Si se introduce un nombre de flujo que ya existe lanza el error.
+        '''
 
         flujo_nombre= request.POST['nombre_flujo']
         flujo_descripcion= request.POST['descripcion_flujo']
@@ -56,10 +104,24 @@ class CrearFlujoConfirm(CrearFlujo):
         return render(request, self.template_name)
 
 class EditarFlujo(FlujoView):
+    '''
+    Esta clase edita flujos en el proyecto.
+
+    Hereda de FlujoView
+
+    template_name es el archivo html de esta clase.
+    '''
 
     template_name = 'EditarFlujo.html'
 
     def post(self, request, *args, **kwargs):
+        '''
+        Esta funcion tiene los parametros:
+        :param request:
+        :param args:
+        :param kwargs:
+        :return: request, el archivo html y el diccionario.
+        '''
         diccionario={}
         #usuario_logueado= Usuario.objects.get(id= request.POST['login'])
         #proyecto_actual= Proyecto.objects.get(id= request.POST['proyecto'])
@@ -73,10 +135,24 @@ class EditarFlujo(FlujoView):
 
 
 class EditarFlujoConfirm(EditarFlujo):
+    '''
+    Esta clase confirma la modificacion de los flujos en el proyecto.
+
+    Hereda de EditarFlujo
+
+    template_name es el archivo html de esta clase.
+    '''
 
     template_name = 'EditarFlujoConfirmar.html'
 
     def post(self, request, *args, **kwargs):
+        '''
+        Esta funcion tiene los parametros:
+        :param request:
+        :param args:
+        :param kwargs:
+        :return: request y el archivo html.
+        '''
 
         flujo_editado = Flujo.objects.get(nombre= request.POST['nombre_flujo'])
         nuevo_flujo_nombre = request.POST['nombre_nuevo_flujo']
@@ -88,10 +164,24 @@ class EditarFlujoConfirm(EditarFlujo):
         return render(request, self.template_name)
 
 class EliminarFlujo(FlujoView):
+    '''
+    Esta clase elimina flujos en el proyecto.
+
+    Hereda de FlujoView
+
+    template_name es el archivo html de esta clase.
+    '''
 
     template_name = 'EliminarFlujo.html'
 
     def post(self, request, *args, **kwargs):
+        '''
+        Esta funcion tiene los parametros:
+        :param request:
+        :param args:
+        :param kwargs:
+        :return: request, el archivo html y el diccionario.
+        '''
 
         diccionario={}
         #usuario_logueado= Usuario.objects.get(id= request.POST['login'])
@@ -109,7 +199,21 @@ class EliminarFlujo(FlujoView):
 
 
 class Actividades(TemplateView):
+    '''
+    Esta clase va a permitir agregar actividades a los flujos en el proyecto.
+
+    Hereda de TemplateView
+
+    template_name es el archivo html de esta clase.
+    '''
     template_name = 'Actividades.html'
     def post(self, request, *args, **kwargs):
+        '''
+        Esta funcion emite el mensaje: En construccion. Tiene los parametros:
+        :param request:
+        :param args:
+        :param kwargs:
+        :return: request y el archivo html.
+        '''
         return render(request, self.template_name)
 
