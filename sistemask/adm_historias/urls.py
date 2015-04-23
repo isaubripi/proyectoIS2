ea60628a37e935b3ac57c91d3a7bc1c4e4db8d24
rubenmed <rubencho.mecab@gmail.com>
2015-04-23 13:06:08 -0400

    ajustes historias de usuario

--- sistemask/adm_historias/urls.py
+++ sistemask/adm_historias/urls.py
@@ -4,12 +4,12 @@ from django.conf.urls import patterns, include, url
 from .views import HistoriaView, CrearHistoria, CrearHistoriaConfirm, EditarHistoria, EditarHistoriaConfirm, EliminarHistoria
 
 urlpatterns= patterns('',
-    url(r'^historia/$', HistoriaView.as_view(), name = 'historia'),
-    url(r'^historia/crear/$', CrearHistoria.as_view(), name = 'crear_historia'),
-    url(r'^historia/crear/confirmar/$', CrearHistoriaConfirm.as_view(), name = 'crear_historia_confirmar'),
-    url(r'^historia/editar/$', EditarHistoria.as_view(), name = 'editar_historia'),
-    url(r'^historia/editar/confirmar/$', EditarHistoriaConfirm.as_view(), name = 'editar_historia_confirmar'),
-    url(r'^historia/eliminar/$', EliminarHistoria.as_view(), name = 'eliminar_historia'),
+    url(r'^$', HistoriaView.as_view(), name = 'historia'),
+    url(r'^crear/$', CrearHistoria.as_view(), name = 'crear_historia'),
+    url(r'^crear/confirmar/$', CrearHistoriaConfirm.as_view(), name = 'crear_historia_confirmar'),
+    url(r'^editar/$', EditarHistoria.as_view(), name = 'editar_historia'),
+    url(r'^editar/confirmar/$', EditarHistoriaConfirm.as_view(), name = 'editar_historia_confirmar'),
+    url(r'^eliminar/$', EliminarHistoria.as_view(), name = 'eliminar_historia'),
 
 
 
