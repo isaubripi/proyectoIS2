b52ea59cdaad7231085c0d6430cbc5aa590333a6
Isidro <isaubripi@gmail.com>
2015-04-24 20:53:34 -0400

    Se corrigio el test.py

--- sistemask/adm_proyectos/tests.py
+++ sistemask/adm_proyectos/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 from adm_usuarios.models import Usuario
-from .models import Proyecto
+from adm_proyectos.models import Proyecto
 
 # Create your tests here.
 class ProyectoTest(TestCase):
@@ -9,32 +9,46 @@ class ProyectoTest(TestCase):
     En es la clase encargara de realizar las pruebas unitarias
     basicamente se prueban la creacion, modificacion y eliminacion
     """
-    print('Inicia pruebas unitarias de ABM PROYECTO')
 
-    def creacion(self):
-        u1 = Usuario.objects.create(username= 'isidro',nombre = 'Isidro', apellido = 'Brizuela', password = 'isidro', cedula = 3841270, email='isaubripi@gmail.com', estado = True)
 
-        Proyecto1 = Proyecto.objects.create(nombre= 'Sistema de informacion 1', descripcion= 'Escrito en java', scrum_master=u1, scrum_team=u1,  fecha_inicio='2015-05-15', fecha_fin='2015-05-17', activo=True )
-        #Proyecto2 = Proyecto.objects.create(nombre= 'Sistema de informacion 2', descripcion= 'Escrito en python', scrum_master= Usuario.objects.create(nombre='Juan'), scrum_team= Usuario.objects.create(nombre='Juan'), fecha_inicio='2015-05-15', fecha_fin='2015-05-17', activo=True )
-        #Proyecto3 = Proyecto.objects.create(nombre= 'Sistema de informacion 3', descripcion= 'Escrito en c++', scrum_master= Usuario.objects.create(nombre='Ruben'), scrum_team= Usuario.objects.create(nombre='Ruben'), fecha_inicio='2015-05-15', fecha_fin='2015-05-17', activo=True )
+    def setUp(self):
+        print(':::::::::::Inicia pruebas unitarias de ABM PROYECTO:::::::::::')
+
+        u1 = Usuario.objects.create(username= 'isidro',nombre = 'Isidro', apellido = 'Brizuela', password = 'isidro', cedula = 3841270)
+
+        Proyecto.objects.create(nombre= 'Sistema de informacion 1', descripcion= 'Escrito en java',scrum_master=u1, fecha_inicio='2015-05-15', fecha_fin='2015-05-17', activo=True )
+        Proyecto.objects.create(nombre= 'Sistema de informacion 2', descripcion= 'Escrito en python',scrum_master=u1, fecha_inicio='2015-05-15', fecha_fin='2015-05-17', activo=True )
+        Proyecto.objects.create(nombre= 'Sistema de informacion 3', descripcion= 'Escrito en c++',scrum_master=u1, fecha_inicio='2015-05-15', fecha_fin='2015-05-17', activo=True )
 
         print('Creacion de Proyectos ejecutada correctamente.')
 
-    '''def modificacion(self):
+    def test_modificacion(self):
         """
         Se modifica el nombre de cada uno de los proyectos creados
         :return:Nada
         """
-        Proyecto1 = Proyecto.objects.update(nombre='Sistema de informacion 3')
-        Proyecto2 = Proyecto.objects.update(nombre='Sistema de informacion 2')
-        Proyecto3 = Proyecto.objects.update(nombre='Sistema de informacion 1')
+        P1 = Proyecto.objects.get(nombre='Sistema de informacion 1')
+        P1.nombre = 'SISTEMA DE INFORMACION 1'
+        P1.save()
+
+        print('Modificacion de Proyectos ejecutada exitosamente')
 
 
-    def eliminacion(self):
+    def test_eliminacion(self):
         """
         Se eliminan los 3 proyectos creados.
         :return:Nada
         """
-        Proyecto1 = Proyecto.objects.delete()
-        Proyecto2 = Proyecto.objects.delete()
-        Proyecto3 = Proyecto.objects.delete()'''
+        P2 = Proyecto.objects.get(nombre= 'Sistema de informacion 2')
+        P2.delete()
+
+        print('Eliminacion de Proyectos ejecutada exitosamente')
+
+    def test_inicializar(self):
+
+        u2 = Usuario.objects.create(username= 'juan',nombre = 'Isidro', apellido = 'Brizuela', password = 'isidro', cedula = 3841270)
+        P3 = Proyecto.objects.get(nombre= 'Sistema de informacion 3')
+
+        P3.scrum_team.add(u2)
+
+        print('Proyecto Inicializado exitosamente')
