7d1e8cbf12b41094f73d7745eac529c59274ea20
Isidro <isaubripi@gmail.com>
2015-04-24 20:55:13 -0400

    Se corrigio el test.py

--- sistemask/adm_roles/tests.py
+++ sistemask/adm_roles/tests.py
@@ -6,14 +6,19 @@ from adm_roles.models import Rol
 from adm_usuarios.models import Usuario
 
 class RolTest(TestCase):
+
     def setUp(self):
+        print(':::::::::::Inicia pruebas unitarias de ABM ROL:::::::::::')
         rol1 = Rol.objects.create(nombre='primerrol')
         rol2 = Rol.objects.create(nombre='segundorol', activo=True, crear_proyecto=True)
+        print('Creacion de roles ejectutada exitosamente')
 
     def test_nombre(self):
         rol1 = Rol.objects.get(nombre = 'primerrol')
         self.assertEqual(rol1.nombre, 'primerrol')
+        print('Comparacion ejecutada exitosamente')
 
-    #def test_usuario(self):
-        #rol2 = Usuario.objects.get(usuario= 'juan')
-        #self.assertEqual(rol2.nombre, 'juan')
+    def test_usuario(self):
+        rol2 = Rol.objects.get(nombre= 'segundorol')
+        self.assertEqual(rol2.nombre, 'segundorol')
+        print('Comparacion ejecutada exitosamente')
